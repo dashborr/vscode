854877764e7a37e581f9e3e36ea0dc1433b6c520
dashborr <danisarfas7@gmail.com>
2026-02-23 16:14:14 +0700

    first commit

---
+++ coba.cpp
@@ -0,0 +1,21 @@
+//contoh library
+#include <iostream>
+using namesapace std;
+
+//variable global
+string nama;
+
+int main(){ //program utama
+
+    int age; //variable local
+    int age1; //variable local
+    age = 20;
+    age = 15;
+
+
+
+    cout << "hallo world" << endl;
+    cout << 99 << endl;
+    cout << age;
+    cout << age1;
+}
